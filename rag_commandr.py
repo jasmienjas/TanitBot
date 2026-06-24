d5eefd58cdfc36c1e96fd89085aa44b2eeec37ad
jasmienjas <yag05@mail.aub.edu>
2026-06-24 19:18:43 +0300

    .

--- rag_commandr.py
+++ rag_commandr.py
@@ -323,7 +323,7 @@ def load_generation_model(model_id, quantization=0, hf_token=None, cache_dir=Non
         bnb_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
-            bnb_4bit_use_double_quant=True,
+            bnb_4bit_use_double_quant=False,
             bnb_4bit_compute_dtype=torch.bfloat16 if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else torch.float16,
             llm_int8_enable_fp32_cpu_offload=True
         )
